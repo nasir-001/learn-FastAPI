b2e2c0b20b46382c587ffd43de918af80ae385ed
Nasir Lawal <nasirlawal001@gmail.com>
2021-03-06 19:48:42 +0100

    request body

--- main.py
+++ main.py
@@ -23,9 +23,21 @@ app = FastAPI()
 async def read_root():
   return {"Hello": "World"}
 
-@app.post("/items")
+@app.post("/items/")
 async def create_item(item: Item):
-  return item
+  item_dict = item.dict()
+  if item.tax:
+    price_with_tax = item.price + item.tax
+    item_dict.update({"price_with_tax": price_with_tax})
+  return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+  result = {"item_id": item_id, **item.dict()}
+  if q:
+    result.update({"q": q})
+  return result
+
 
 @app.get("/items/{item_id}")
 async def get_user_item(item_id:  str, needy: str):

b5e525cf17ca98a54360056d49b90c38e0b96da8
Nasir Lawal <nasirlawal001@gmail.com>
2021-03-08 10:40:52 +0100

    schema extra - example

--- main.py
+++ main.py
@@ -61,7 +61,15 @@ async def create_item(item: Item):
   return item_dict
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item = Body(
+  ...,
+  example={
+    "name": "Foo",
+    "description": "A very nice description",
+    "price": 35.4,
+    "tax": 3.2
+  }
+)):
   results = {"item_id": item_id, "item": item}
   return results
 

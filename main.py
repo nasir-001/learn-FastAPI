bf1a3e9d5d5dbd2f79d72fe7112a8e03fa1e003b
Nasir Lawal <nasirlawal001@gmail.com>
2021-03-08 12:45:56 +0100

    extra data types

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime, time, timedelta
+from uuid import UUID
 from fastapi import FastAPI, Query
 from enum import Enum
 from typing import Dict, List, Set, Optional
@@ -61,17 +63,26 @@ async def create_item(item: Item):
   return item_dict
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item = Body(
-  ...,
-  example={
-    "name": "Foo",
-    "description": "A very nice description",
-    "price": 35.4,
-    "tax": 3.2
+async def update_item(
+  item_id: UUID,
+  start_datetime: Optional[datetime] = Body(None),
+  end_datetime: Optional[datetime] = Body(None),
+  repeat_at: Optional[time] = Body(None),
+  process_after: Optional[timedelta]= Body(None)
+):
+
+  start_process = start_datetime + process_after
+  duration = end_datetime - start_process
+
+  return {
+    "Item_id": item_id,
+    "start_datetime": start_datetime,
+    "end_datetime": end_datetime,
+    "repeat_at": repeat_at,
+    "process_after": process_after,
+    "start_process": start_process,
+    "duration": duration
   }
-)):
-  results = {"item_id": item_id, "item": item}
-  return results
 
 @app.get('/users/me')
 async def read_user_me():
